a027115160a85e3b3451d8fad628b6acb412db24
peterchencc <peterchencc@gmail.com>
2015-05-13 02:42:27 +0800

    Fix API query and some columns required

--- app/controllers/api_v1/coaches_controller.rb
+++ app/controllers/api_v1/coaches_controller.rb
@@ -1,11 +1,11 @@
 class ApiV1::CoachesController < ApiController
 
   def index
-    @coaches = Coach.all.order("created_at DESC")
+    @coaches = Coach.all.order("created_at DESC").includes(:sports, :certificates, :experiences, :lessons, :skills)
   end
 
   def show
-    @i = params[:id]
+    @coach = params[:id]
     render :partial => "api_v1/coaches/show.json"
   end
 
