aefd62989cae891d00960576327bb6f0559e9ee9
peterchencc <peterchencc@gmail.com>
2015-05-15 14:41:08 +0800

    Add coaches tab bar

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -7,7 +7,14 @@ class CoachesController < ApplicationController
   end
 
   def index
-    @coaches = Coach.all.order("created_at DESC").includes(:sports, :certificates, :experiences, :lessons, :skills)
+    if params[:sports]
+      sport = Sport.find_by_name(params[:sports])
+      @coaches = sport.coaches.order("created_at DESC").includes(:sports, :certificates, :experiences, :lessons, :skills)
+    else
+      @coaches = Coach.all.order("created_at DESC").includes(:sports, :certificates, :experiences, :lessons, :skills)
+    end
+    @coaches_all = Coach.count
+    @sports = Sport.all
   end
 
   def show
