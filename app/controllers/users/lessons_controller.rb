77c49c90bf64b8f442306a1c8f3ee7017d23d664
peterchencc <peterchencc@gmail.com>
2015-05-16 13:22:24 +0800

    Coach can edit lesson

--- app/controllers/users/lessons_controller.rb
+++ app/controllers/users/lessons_controller.rb
@@ -8,6 +8,20 @@ class Users::LessonsController < ApplicationController
     @lessons = current_user.coach.lessons.all.order("created_at DESC")
   end
 
+  def edit
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def update
+    @lesson = Lesson.find(params[:id])
+    if @lesson.update( lesson_params )
+      # flash[:notice] = "編輯成功"
+      redirect_to users_coach_lessons_path
+    else
+      render :action => :edit
+    end
+  end
+
   def create
     @coach = current_user.coach
     @lesson = @coach.lessons.create(lesson_params)
